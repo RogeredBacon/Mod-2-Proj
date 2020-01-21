40b660f002e28754a0ed55943bd711c0f25642eb
RogeredBacon <github email address>
2020-01-21 18:22:35 +0000

    Finally fixed, pretty much, FML, it was the first thing we tried earlier today...

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
     has_many :document_access
     has_many :documents, through: :document_access
-    has_many :friendships, foreign_key: 'user'
-    has_many :friendships, foreign_key: 'friend_user'
+    # has_many :friendships, foreign_key: 'user'
+    # has_many :friendships, foreign_key: 'friend_user'
+    has_many :friendships, ->(user) { where('user_id = ? OR friend_user_id = ?', user.id, user.id)}
     has_many :users, through: :friendships, foreign_key: 'friend_user'
     has_many :messages, through: :friendships
 end
