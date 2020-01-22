c4549e40373a29bee5c679df3b062c1f4008bc73
RogeredBacon <github email address>
2020-01-22 10:12:56 +0000

    Added ckeditor from other repo

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,2 +1,29 @@
 class DocumentsController < ApplicationController
+    def index
+        @documents = Document.all
+    end
+
+    def show
+        @document = Document.find(params[:id])
+    end
+
+    def new
+        @document = Document.new
+    end
+
+    def create
+        @document = Document.create(data: params[:data])
+        redirect_to document_path
+    end
+
+    def edit
+        @document = Document.find(params[:id])
+    end
+
+    def update
+        @document = Document.find(params[:id])
+        @document.update(data: params[:document][:data])
+        redirect_to document_path
+    end
+
 end
