ec2b88997d5fb6f5c17bd0803a21c993e4527895
RogeredBacon <github email address>
2020-01-23 14:28:50 +0000

    Final draft messages

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,6 +1,7 @@
 class FriendshipsController < ApplicationController
     def index
         @friendships = Friendship.all.where("user_id = ? OR friend_user_id = ?", current_user.id, current_user.id)
+        @friendships = @friendships.select{|f| f.confirmed?}
     end
 
     def new
